4ff6721cf44933b0f37db9ef7d7b96639736c15f
Geoffrey Biggs <git@killbots.net>
2010-11-25 18:07:58 +0900

    Revised rtmgr command line

--- rtshell/rtmgr.py
+++ rtshell/rtmgr.py
@@ -19,24 +19,19 @@ Implementation of the command for controlling managers.
 '''
 
 
-from optparse import OptionParser, OptionError
+import optparse
 import os
-from rtctree.exceptions import RtcTreeError, FailedToLoadModuleError, \
-                               FailedToUnloadModuleError, \
-                               FailedToCreateComponentError, \
-                               FailedToDeleteComponentError
-from rtctree.tree import create_rtctree, InvalidServiceError, \
-                         FailedToNarrowRootNamingError, \
-                         NonRootPathError
-from rtctree.path import parse_path
+import rtctree.exceptions
+import rtctree.tree
+import rtctree.path
 import sys
 
-from rtshell import RTSH_PATH_USAGE, RTSH_VERSION
-from rtshell.path import cmd_path_to_full_path
+import rtshell
+import rtshell.path
 
 
 def get_manager(cmd_path, full_path, tree=None):
-    path, port = parse_path(full_path)
+    path, port = rtctree.path.parse_path(full_path)
     if port:
         # Can't configure a port
         print >>sys.stderr, '{0}: Cannot access {1}: No such \
@@ -48,7 +43,7 @@ object.'.format(sys.argv[0], cmd_path)
         path = path[:-1]
 
     if not tree:
-        tree = create_rtctree(paths=path, filter=[path])
+        tree = rtctree.tree.create_rtctree(paths=path, filter=[path])
     if not tree:
         return None
 
@@ -74,7 +69,7 @@ def load_module(cmd_path, full_path, module_path, init_func, tree=None):
 
     try:
         mgr.load_module(module_path, init_func)
-    except FailedToLoadModuleError:
+    except rtctree.exceptions.FailedToLoadModuleError:
         print >>sys.stderr, '{0}: Failed to load module {1}'.format(\
                 sys.argv[0], module_path)
         return 1
@@ -89,7 +84,7 @@ def unload_module(cmd_path, full_path, module_path, tree=None):
 
     try:
         mgr.unload_module(module_path)
-    except FailedToUnloadModuleError:
+    except rtctree.exceptions.FailedToUnloadModuleError:
         print >>sys.stderr, '{0}: Failed to unload module {1}'.format(\
                 sys.argv[0], module_path)
         return 1
@@ -104,7 +99,7 @@ def create_component(cmd_path, full_path, module_name, tree=None):
 
     try:
         mgr.create_component(module_name)
-    except FailedToCreateComponentError:
+    except rtctree.exceptions.FailedToCreateComponentError:
         print >>sys.stderr, '{0}: Failed to create component from module \
 {1}'.format(sys.argv[0], module_name)
         return 1
@@ -119,7 +114,7 @@ def delete_component(cmd_path, full_path, instance_name, tree=None):
 
     try:
         mgr.delete_component(instance_name)
-    except FailedToDeleteComponentError, e:
+    except rtctree.exceptions.FailedToDeleteComponentError, e:
         print >>sys.stderr, '{0}: Failed to delete component {1}'.format(\
                 sys.argv[0], instance_name)
         return 1
@@ -128,75 +123,69 @@ def delete_component(cmd_path, full_path, instance_name, tree=None):
 
 
 def main(argv=None, tree=None):
-    usage = '''Usage: %prog [options] <path> <command> [args]
+    usage = '''Usage: %prog [options] <path>
 Control a manager, adding and removing shared libraries and components. To
-set a mananger's configuration, use rtconf.
+set a mananger's configuration, use rtconf. To view a manager's information,
+use rtcat.
 
-A command should be one of:
-    load, unload, create, delete
+If multiple commands are given, they are executed in the order unload, load,
+delete, create.
 
-load <file system path> <init function>
-Load a shared library (DLL file or .so file) into the manager.
-
-unload <file system path>
-Unload a shared library (DLL file or .so file) from the manager.
-
-create <module name>
-Create a new component instance from a loaded shared library.
-Properties of the new component can be set by specifying them as part of the
-module name argument, prefixed by a question mark. For example, to set the
-instance name of a new component of type ConsoleIn, use:
-    rtmgr manager.mgr create ConsoleIn?instance_name=blag
-
-delete <instance name>
-Delete a component instance from the manager, destroying it.
-
-''' + RTSH_PATH_USAGE
-    version = RTSH_VERSION
-    parser = OptionParser(usage=usage, version=version)
+''' + rtshell.RTSH_PATH_USAGE
+    version = rtshell.RTSH_VERSION
+    parser = optparse.OptionParser(usage=usage, version=version)
+    parser.add_option('-c', '--create', dest='mod_name', action='store',
+            type='string', default='', help='Create a new component instance '\
+            'from the specified loaded module. Properties of the new '\
+            'component an be specified after the module name prefixed with a '\
+            'question mark. e.g. ConsoleIn?instance_name=blag')
+    parser.add_option('-d', '--delete', dest='instance_name', action='store',
+            type='string', default='', help='Shut down and delete the '\
+            'specified component instance.')
+    parser.add_option('-i', '--init-func', dest='init_func', action='store',
+            type='string', default='', help='Initialisation function for use '\
+            'with the --load option.')
+    parser.add_option('-l', '--load', dest='mod_path', action='store',
+            type='string', default='', help='Load the module into the '\
+            'manager. An initialisation function must be specified using '\
+            'the --init-func option.')
+    parser.add_option('-u', '--unload', dest='mod_path_u', action='store',
+            type='string', default='', help='Unload the module from the '\
+            'manager.')
 
     if argv:
         sys.argv = [sys.argv[0]] + argv
     try:
         options, args = parser.parse_args()
-    except OptionError, e:
+    except optparse.OptionError, e:
         print >>sys.stderr, 'OptionError:', e
         return 1
 
-    if len(args) > 2:
-        cmd_path = args[0]
-        cmd = args[1]
-        args = args[2:]
-    else:
-        print >>sys.stderr, usage
+    if len(args) != 1:
+        print >>sys.stderr, '{0}: No manager specified.'.format(sys.argv[0])
         return 1
-    full_path = cmd_path_to_full_path(cmd_path)
+    full_path = rtshell.path.cmd_path_to_full_path(args[0])
 
-    if cmd == 'load':
-        if len(args) != 2:
-            print >>sys.stderr, '{0}: Incorrect number of arguments for load \
-command.'.format(sys.argv[0])
+    if options.mod_path_u:
+        # Unload a module
+        return unload_module(args[0], full_path, options.mod_path_u, tree)
+    if options.mod_path:
+        # Load a module
+        if not options.init_func:
+            print >>sys.stderr, '{0}: No initialisation function '\
+                    'specified.'.format(sys.argv[0])
             return 1
-        return load_module(cmd_path, full_path, args[0], args[1], tree)
-    elif cmd == 'unload':
-        if len(args) != 1:
-            print >>sys.stderr, '{0}: Incorrect number of arguments for \
-unload command.'.format(sys.argv[0])
-            return 1
-        return unload_module(cmd_path, full_path, args[0], tree)
-    elif cmd == 'create':
-        if len(args) != 1:
-            print >>sys.stderr, '{0}: Incorrect number of arguments for \
-create command.'.format(sys.argv[0])
-            return 1
-        return create_component(cmd_path, full_path, args[0], tree)
-    elif cmd == 'delete':
-        if len(args) != 1:
-            print >>sys.stderr, '{0}: Incorrect number of arguments for \
-delete command.'.format(sys.argv[0])
-            return 1
-        return delete_component(cmd_path, full_path, args[0], tree)
+        return load_module(args[0], full_path, options.mod_path,
+                options.init_func, tree)
+    if options.instance_name:
+        # Delete a component
+        return delete_component(args[0], full_path, options.instance_name,
+                tree)
+    if options.mod_name:
+        # Create a component
+        return create_component(args[0], full_path, options.mod_name, tree)
 
+    print >>sys.stderr, '{0}: No actions specified.'.format(sys.argv[0])
     print >>sys.stderr, usage
     return 1
 

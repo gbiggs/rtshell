6474b1cad6132a096b9a3ead94543e31449897ca
gbiggs <git@killbots.net>
2010-11-29 19:40:19 +0900

    Basic recording and playback complete

--- rtshell/rtlog.py
+++ rtshell/rtlog.py
@@ -14,7 +14,7 @@ Copyright (C) 2009-2010
 Licensed under the Eclipse Public License -v 1.0 (EPL)
 http://www.opensource.org/licenses/eclipse-1.0.txt
 
-Implementation of the command to print data sent by ports to the console.
+Implementation of the logging command.
 
 '''
 
@@ -30,65 +30,80 @@ import time
 import traceback
 
 import comp_mgmt
-import eval_const
+import modmgr
 import path
 import port_types
 import rtlog_comps
 import rtshell
-import user_mods
+import simpkl_log
 
 
 def record_log(raw_paths, options, tree=None):
     event = threading.Event()
 
-    if options.timeout == -1:
-        if options.number == 0:
-            if options.end != -1:
-                print >>sys.stderr, 'Record until {0}.'.format(options.end)
-        else:
-            print >>sys.stderr, 'Record {0} entries.'.format(options.number)
-    else:
-        print >>sys.stderr, 'Record for {0}s'.format(options.timeout)
-
-    sources = port_types.parse_targets(raw_paths)
-    if not tree:
-        paths = [t[0] for s in sources]
-        tree = rtctree.tree.create_rtctree(paths=paths, filter=paths)
-    port_specs = port_types.make_port_specs(sources, mods, tree)
-    port_types.require_all_input(port_specs)
+    mm = modmgr.ModuleMgr(verbose=options.verbose)
+    mm.load_mods_and_poas(options.modules)
     if options.verbose:
         print >>sys.stderr, \
-                'Port specifications: {0}'.format([str(p) for p in port_specs])
+                'Pre-loaded modules: {0}'.format(mm.loaded_mod_names)
 
-    if not options.filename:
-        options.filename = 'rtlog_{0}.rtl'.format(time.time())
+    if options.timeout != -1:
+        print >>sys.stderr, 'Record for {0}s'.format(options.timeout)
+    else:
+        if options.end != -1:
+            if options.index:
+                print >>sys.stderr, 'Record {0} entries.'.format(
+                        options.end)
+            else:
+                print >>sys.stderr, 'Record until {0}.'.format(options.end)
+
     if options.logger == 'simpkl':
-        logger = simpkl_log.SimplePickleLog(filename=options.filename,
-                mode='w', meta=port_specs, verbose=options.verbose)
+        l_type = simpkl_log.SimplePickleLog
     elif options.logger == 'text':
-        logger = textlog.TextLogger(filename=options.filename,
-                mode='w', verbose=options.verbose)
+        l_type = textlog.TextLogger
     else:
         print >>sys.stderr, '{0}: Invalid logger type: {1}',format(sys.argv[0],
                 options.logger)
         return 1
 
+    sources = port_types.parse_targets(raw_paths)
+    if not tree:
+        paths = [s[0] for s in sources]
+        tree = rtctree.tree.create_rtctree(paths=paths, filter=paths)
+    port_specs = port_types.make_port_specs(sources, mm, tree)
+    port_types.require_all_input(port_specs)
+    if options.verbose:
+        print >>sys.stderr, \
+                'Port specifications: {0}'.format([str(p) for p in port_specs])
+
     comp_name, mgr = comp_mgmt.make_comp('rtlog_recorder', tree,
             rtlog_comps.Recorder, port_specs, event=event,
-            logger=logger, lims_are_ind=options.index, end=options.end)
+            logger_type=l_type, filename=options.filename,
+            lims_are_ind=options.index, end=options.end,
+            verbose=options.verbose)
     if options.verbose:
         print >>sys.stderr, 'Created component {0}'.format(comp_name)
-    comp = comp_mgmt.find_comp_in_mgr(comp_name, mgr)
-    comp_mgmt.connect(comp, port_specs, tree)
-    comp_mgmt.activate(comp)
+    try:
+        comp = comp_mgmt.find_comp_in_mgr(comp_name, mgr)
+        comp_mgmt.connect(comp, port_specs, tree)
+        comp_mgmt.activate(comp)
+    except Exception, e:
+        #comp_mgmt.shutdown(mgr)
+        raise e
     try:
         if options.timeout != -1:
             event.wait(options.timeout)
             comp_mgmt.disconnect(comp)
             comp_mgmt.deactivate(comp)
+        elif options.end > -1:
+            event.wait()
+            comp_mgmt.disconnect(comp)
+            comp_mgmt.deactivate(comp)
         else:
             while True:
                 raw_input()
+            # The manager will catch the Ctrl-C and shut down itself, so don't
+            # disconnect/deactivate the component
     except KeyboardInterrupt:
         pass
     except EOFError:
@@ -102,43 +117,77 @@ def record_log(raw_paths, options, tree=None):
 def play_log(raw_paths, options, tree=None):
     event = threading.Event()
 
-    if options.timeout == -1:
-        if options.number == 0:
-            if options.end != -1:
-                print >>sys.stderr, 'Play from {0} until {1}.'.format(
-                        options.start, options.end)
-        else:
-            print >>sys.stderr, 'Play {0} entries.'.format(options.number)
-    else:
-        print >>sys.stderr, 'Play for {0}s'.format(options.timeout)
+    if not options.filename:
+        print >>sys.stderr, \
+                '{0}: No log file specified.'.format(sys.argv[0])
+        return 1
 
+    mm = modmgr.ModuleMgr(verbose=options.verbose)
+    mm.load_mods_and_poas(options.modules)
+    if options.verbose:
+        print >>sys.stderr, \
+                'Pre-loaded modules: {0}'.format(mm.loaded_mod_names)
+
+    if options.timeout != -1:
+        print >>sys.stderr, 'Play for {0}s'.format(options.timeout)
+    else:
+        if options.end != -1:
+            if options.index:
+                print >>sys.stderr, 'Play {0} entries.'.format(
+                        options.end)
+            else:
+                print >>sys.stderr, 'Play until {0}.'.format(options.end)
+
+    if options.logger == 'simpkl':
+        l_type = simpkl_log.SimplePickleLog
+    elif options.logger == 'text':
+        print >>sys.stderr, '{0}: Logger type "text" does not support '\
+                'playback.'.format(sys.argv[0])
+        return 1
+    else:
+        print >>sys.stderr, '{0}: Invalid logger type: {1}',format(sys.argv[0],
+                options.logger)
+        return 1
+
+    # TODO: allow port specs with multiple targets to be connected to.
     targets = port_types.parse_targets(raw_paths)
     if not tree:
         paths = [t[0] for t in targets]
         tree = rtctree.tree.create_rtctree(paths=paths, filter=paths)
-    port_specs = port_types.make_port_specs(targets, mods, tree)
+    port_specs = port_types.make_port_specs(targets, mm, tree)
     port_types.require_all_output(port_specs)
     if options.verbose:
         print >>sys.stderr, \
                 'Port specifications: {0}'.format([str(p) for p in port_specs])
 
     comp_name, mgr = comp_mgmt.make_comp('rtlog_player', tree,
-            rtlog_comps.Player, port_specs, event=event,
-            limits_are_ind=options.index, start=options.start,
-            end=options.end_time, num=options.number)
+            rtlog_comps.Player, port_specs, event=event, logger_type=l_type,
+            filename=options.filename, lims_are_ind=options.index,
+            start=options.start, end=options.end, rate=options.rate,
+            abs_times=options.abs_times, ignore_times=options.ig_times,
+            verbose=options.verbose)
     if options.verbose:
         print >>sys.stderr, 'Created component {0}'.format(comp_name)
     comp = comp_mgmt.find_comp_in_mgr(comp_name, mgr)
     comp_mgmt.connect(comp, port_specs, tree)
     comp_mgmt.activate(comp)
+    #if comp_mgmt.activate(comp) != RTC.RTC_OK:
+        #print >>sys.stderr, '{0}: Error activating player.'.format(sys.argv[0])
+        #return 1
     try:
         if options.timeout != -1:
             event.wait(options.timeout)
             comp_mgmt.disconnect(comp)
             comp_mgmt.deactivate(comp)
+        elif options.end > -1:
+            event.wait()
+            comp_mgmt.disconnect(comp)
+            comp_mgmt.deactivate(comp)
         else:
             while True:
                 raw_input()
+            # The manager will catch the Ctrl-C and shut down itself, so don't
+            # disconnect/deactivate the component
     except KeyboardInterrupt:
         pass
     except EOFError:
@@ -170,52 +219,60 @@ settings compatible with the port.'''
             help='Times from the logged data are sent as recorded during '\
             'replay, rather than adjusted to the current timeframe. '\
             '[Default: %default]')
-    parser.add_option('-e', '--end', dest='end', action='store', type='string',
-            default='-1',
+    parser.add_option('-d', '--display-info', dest='display_info',
+            action='store_true', default=False, help='Display the log '\
+            'information and exit.')
+    parser.add_option('-e', '--end', dest='end', action='store', type='float',
+            default=-1,
             help='Time or entry index to stop recording or playback. Must be '\
-                    'within the bounds of the log. Specify -1 to record '\
-                    'forever or replay to the end of the log. Use --index to '\
-                    'specify that this value is an index. [Default: %default]')
+            'within the bounds of the log. Specify -1 to record forever or '\
+            'replay to the end of the log. Use --index to specify that this '\
+            'value is an index. [Default: %default]')
     parser.add_option('-f', '--filename', dest='filename', action='store',
             type='string', default='', help='File name of the log file to '\
-                    'record to/playback from. If not specified for '\
-                    'recording, a default will be created based on the '\
-                    'current time. Must be specified for playback.')
+            'record to/playback from. If not specified for recording, a '\
+            'default will be created based on the current time. Must be '\
+            'specified for playback.')
     parser.add_option('-i', '--index', dest='index', action='store_true',
             default=False, help='Interpret the start and end values as entry '\
-                    'indices. [Default: %default]')
+            'indices. [Default: %default]')
     parser.add_option('-l', '--logger', dest='logger', action='store',
             type='string', default='simpkl', help='The type of logger to '\
-                    'use. The default is the SimplePickle logger. '\
-                    'Alternatively, the text logger (specify using "text") '\
-                    'may be used. The text logger does not support playback.')
-    parser.add_option('-m', '--type-mod', dest='type_mods', action='store',
-            type='string', default='',
-            help='Specify the module containing the data type. This option '\
-                    'must be supplied if the data type is not defined in the '\
-                    'RTC modules supplied with OpenRTM-aist. This module and '\
-                    'the __POA module will both be imported.')
+            'use. The default is the SimplePickle logger. Alternatively, '\
+            'the text logger (specify using "text") may be used. The text '\
+            'logger does not support playback.')
+    parser.add_option('-m', '--mod', dest='modules', action='append',
+            type='string', default=[],
+            help='Extra modules to import. If automatic module loading '\
+            'struggles with your data types, try listing the modules here. '\
+            'The module and its __POA partner will be imported.')
+    parser.add_option('-n', '--ignore-times', dest='ig_times',
+            action='store_true', default=False, help='Ignore the log '\
+            'timestamps and play back a fixed number of entries per '\
+            'execution. Use --rate to change the number played back per '\
+            'execution. The value of --rate will be treated as an integer '\
+            'in this case.')
     parser.add_option('-p', '--play', dest='play', action='store_true',
             default=False, help='Replay mode. [Default: %default]')
     parser.add_option('-r', '--rate', dest='rate', action='store',
             type='float', default=1.0,
             help='Scale the playback speed of the log. [Default: %default]')
     parser.add_option('-s', '--start', dest='start', action='store',
-            type='string', default='0.0',
+            type='float', default=0,
             help='Time or entry index to start playback from. Must be within '\
-                    'the bounds of the log. Use --index to specify that this '\
-                    'value is an index. [Default: %default]')
+            'the bounds of the log. Use --index to specify that this value '\
+            'is an index. [Default: %default]')
     parser.add_option('-t', '--timeout', dest='timeout', action='store',
             type='float', default=-1, help='Record/replay data for this many '\
-                    'seconds. Specify -1 for no timeout. This option '\
-                    'overrides --number. [Default: %default]')
+            'seconds. Specify -1 for no timeout. This option overrides '\
+            '--number. [Default: %default]')
     parser.add_option('-v', '--verbose', dest='verbose', action='store_true',
             default=False,
             help='Output verbose information. [Default: %default]')
     parser.add_option('-x', '--exec-rate', dest='exec_rate', action='store',
             type='float', default=10.0,
             help='Specify the rate in Hertz at which to run the component. '\
-                    '[Default: %default]')
+            '[Default: %default]')
 
     if argv:
         sys.argv = [sys.argv[0]] + argv
@@ -230,13 +287,16 @@ settings compatible with the port.'''
         return 1
 
     try:
-        if options.play:
-            result = replay_log([path.cmd_path_to_full_path(p) for p in args],
+        if options.display_info:
+            result = display_info(options)
+        elif options.play:
+            result = play_log([path.cmd_path_to_full_path(p) for p in args],
                     options, tree)
         else:
             result = record_log([path.cmd_path_to_full_path(p) for p in args],
                     options, tree)
     except Exception, e:
+        traceback.print_exc()
         print >>sys.stderr, '{0}: {1}'.format(sys.argv[0], e)
         return 1
     return result
--- rtshell/rtlog_comps.py
+++ rtshell/rtlog_comps.py
@@ -22,6 +22,8 @@ Log recording and playing components used by rtlog
 import OpenRTM_aist
 import RTC
 import sys
+import time
+import traceback
 
 import gen_comp
 import ilog
@@ -30,44 +32,191 @@ import ilog
 ###############################################################################
 ## Recorder component for rtlog
 
-class LogWriter(gen_comp.GenComp):
-    def __init__(self, mgr, port_specs, filename='', lims_are_ind=False,
-            end=-1, *args, **kwargs):
+class Recorder(gen_comp.GenComp):
+    def __init__(self, mgr, port_specs, logger_type=None, filename='',
+            lims_are_ind=False, end=-1, verbose=False, *args, **kwargs):
         if lims_are_ind:
             max = end
             self._end = -1
         else:
             max = -1
             self._end = end
+        try:
+            del kwargs['max']
+        except KeyError:
+            pass
         gen_comp.GenComp.__init__(self, mgr, port_specs, max=max, *args,
                 **kwargs)
+        self._logger_type = logger_type
         self._fn = filename
+        self._verb = verbose
 
-    def on_activated(self, ec_id):
+    def onActivated(self, ec_id):
+        start = time.time()
         # Add activated time to meta data
-        # Add port spec to meta data (include names)
+        # Add port specs to meta data (include names)
+        meta = (start, self._port_specs)
         # Make file name from activated time
+        if not self._fn:
+            self._fn = 'rtlog_{0}.rtl'.format(int(start))
         # Create log, record meta data
+        self._l = self._logger_type(filename=self._fn, mode='w', meta=meta, verbose=self._verb)
+        return RTC.RTC_OK
 
-    def on_deactivated(self, ec_id):
+    def onDeactivated(self, ec_id):
         # Finalise and close log
+        self._l.close()
+        return RTC.RTC_OK
 
     def _behv(self, ec_id):
-        execed = False
+        execed = 0
         result = RTC.RTC_OK
-        for p in self._ports:
+        for name in self._ports:
+            p = self._ports[name]
             if p.port.isNew():
-                execed = True
+                execed += 1
                 p.read()
-                if not self._log(p):
-                    result = RTC.RTC_ERROR
+                ts = self._log(p, name)
+                if self._end > -1 and ts >= self._end:
+                    # Reached the end time
+                    self._set()
+            if self._max > -1 and self._count >= self._max:
+                # Reached the max entries
+                return result, execed
         return result, execed
 
-    def _log(self, port):
+    def _log(self, port, port_name):
         if port.standard_type:
-            ts = 
-            data_time = port.data.tm
+            ts = ilog.EntryTS(sec=port.data.tm.sec, nsec=port.data.tm.nsec)
         else:
+            ts = ilog.EntryTS(time=time.time())
+        self._l.write(ts, (port_name, port.data))
+        return ts
+
+
+###############################################################################
+## Player component for rtlog
+
+class Player(gen_comp.GenComp):
+    def __init__(self, mgr, port_specs, logger_type=None, filename='',
+            lims_are_ind=False, start=-1, end=-1, rate=1.0, abs_times=False,
+            ignore_times=False, verbose=False, *args, **kwargs):
+        if lims_are_ind:
+            max = end
+            self._end = -1
+        else:
+            max = -1
+            self._end = end
+        try:
+            del kwargs['max']
+        except KeyError:
             pass
-        self._l.write(data)
+        gen_comp.GenComp.__init__(self, mgr, port_specs, max=max, *args,
+                **kwargs)
+        self._logger_type = logger_type
+        self._fn = filename
+        self._start = start
+        self._rate = rate
+        self._abs = abs_times
+        self._ig_times = ignore_times
+        self._verb = verbose
+
+    def onActivated(self, ec_id):
+        try:
+            self._l = self._logger_type(filename=self._fn, mode='r',
+                    verbose=self._verb)
+            # Read the metadata block
+            start, log_port_specs = self._l.metadata
+            self._vprint('Log started at {0}'.format(start))
+            self._vprint('Log port specs are {0}'.format(
+                [str(s) for s in log_port_specs]))
+            self._offset = time.time() - start
+            self._vprint('Time offset is {0}'.format(self._offset))
+
+            # Check ports match
+            for name in self._ports:
+                matches = [s for s in log_port_specs if s.name == name]
+                if len(matches) == 0:
+                    print >>sys.stderr, 'WARNING: Port {0} not found in '\
+                            'log.'.format(name)
+                    continue
+                elif len(matches) != 1:
+                    print >>sys.stderr, 'WARNING: Port {0} occurs multiple '\
+                            'times in the log.'.format(name)
+                    continue
+                m = matches[0]
+                if m.type != self._ports[name].raw.type:
+                    print >>sys.stderr, 'ERROR: Port {0} is incorrect data '\
+                            'type; should be {1}.'.format(name,
+                                    type(self._ports[name].data))
+                    return RTC.RTC_ERROR
+
+            # Fast-forward to the start time
+        except:
+            traceback.print_exc()
+            return RTC.RTC_ERROR
+        return RTC.RTC_OK
+
+    def onDeactivated(self, ec_id):
+        # Close log
+        self._l.close()
+        return RTC.RTC_OK
+
+    def _behv(self, ec_id):
+        execed = 0
+        result = RTC.RTC_OK
+        try:
+            if self._ig_times:
+                # Read self._rate items from the log and write them
+                for ii in range(int(self._rate)):
+                    self._vprint('Playing {0} entries.'.format(int(self._rate)))
+                    if not self._pub_log_item():
+                        print >>sys.stderr, '{0}: End of log reached.'.format(
+                                sys.argv[0])
+                        self._set()
+                        result = RTC.RTC_ERROR
+                    else:
+                        execed += 1
+            else:
+                # Calculate the current time in log-time
+                now = (time.time() - self._offset) * self._rate
+                self._vprint('Current time in logspace is {0}'.format(now))
+                # Read until past it - read one at a time to avoid huge memory
+                # spikes if the log contains large-sized data
+                while self._l.pos[1] <= now:
+                    if self._max > -1 and execed >= self._max:
+                        self._vprint(
+                                'Reached maximum number of results to play.')
+                        break
+                    if self._end > -1 and self._l.pos[1] > self._end:
+                        self._bprint('Reached end time (current position: '\
+                                '{0}).'.format(self._l.pos))
+                        break
+                    if not self._pub_log_item():
+                        print >>sys.stderr, '{0}: End of log reached.'.format(
+                                sys.argv[0])
+                        self._set()
+                        result = RTC.RTC_ERROR
+                        break
+                    else:
+                        execed += 1
+        except:
+            traceback.print_exc()
+            return RTC.RTC_ERROR, 0
+        return result, execed
+
+    def _pub_log_item(self):
+        # Read an item from the log file
+        entries = self._l.read()
+        if len(entries) == 0:
+            return False # End of file
+        index, ts, entry = entries[0]
+        p_name, data = entry
+        if p_name in self._ports:
+            self._ports[p_name].port.write(data)
+        return True
+
+    def _vprint(self, text):
+        if self._verb:
+            print >>sys.stderr, text
 

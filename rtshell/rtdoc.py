9982e60b3ca5d661490b1a55690eb253c44fcbae
Geoffrey Biggs <git@killbots.net>
2010-12-27 09:04:44 +0900

    Code clean up and small changes

--- rtshell/rtdoc.py
+++ rtshell/rtdoc.py
@@ -13,18 +13,17 @@ Copyright (C) 2009-2010
 Licensed under the Eclipse Public License -v 1.0 (EPL)
 http://www.opensource.org/licenses/eclipse-1.0.txt
 
-Implementation of the command to display component document.
+Implementation of the command to display component documentation.
 
 '''
 
 
+import docutils.core
 import optparse
 import os
-import rtctree.exceptions
+import os.path
 import rtctree.tree
 import rtctree.path
-import rtctree.utils
-import SDOPackage
 import sys
 import traceback
 
@@ -32,11 +31,10 @@ import path
 import rts_exceptions
 import rtshell
 
-from docutils.core import publish_string
-
 
 def escape(s):
     return s.replace('"', "'")
+
 
 def section(s, level=0):
     result = []
@@ -48,30 +46,35 @@ def section(s, level=0):
         result.append('-' * len(s))
     return result
 
-def format_ports(comp):
+
+def get_ports_docs(comp):
     result = []
     result.append('.. csv-table:: Ports')
     result.append('   :header: "Name", "Type", "DataType", "Description"')
     result.append('   :widths: 8, 8, 8, 26')
     result.append('   ')
     for p in comp.ports:
-        datatype = p.properties['dataport.data_type']
+        if p.porttype == 'DataInPort' or p.porttype == 'DataOutPort':
+            datatype = p.properties['dataport.data_type']
+        else:
+            datatype = ''
         try:
             description = p.properties['description']
         except KeyError:
             description = ''
-        result.append('   "{0}", "{1}", "{2}", "{3}"'.format(p.name, p.porttype, datatype, escape(description)))
+        result.append('   "{0}", "{1}", "{2}", "{3}"'.format(p.name,
+            p.porttype, datatype, escape(description)))
     return result
 
 
-def format_properties(comp):
+def get_props_docs(comp):
     result = []
     result.append('.. csv-table:: Configration parameters')
     result.append('   :header: "Name", "Description"')
     result.append('   :widths: 12, 38')
     result.append('   ')
 
-    for n, v in comp.conf_sets['default'].data.iteritems():
+    for n in comp.conf_sets['default'].data:
         try:
             description = comp.conf_sets['__description__'].data[n]
         except KeyError:
@@ -80,15 +83,15 @@ def format_properties(comp):
     return result
 
 
-def format_component(object, tree):
+def get_comp_docs(object, tree):
     result = []
     result += section(object.name, 0)
     result.append(object.description)
     result.append('')
 
-    result.append(':{0}: {1}'.format('Vendor', object.vendor))
-    result.append(':{0}: {1}'.format('Version', object.version))
-    result.append(':{0}: {1}'.format('Category', object.category))
+    result.append(':Vendor: {0}'.format(object.vendor))
+    result.append(':Version: {0}'.format(object.version))
+    result.append(':Category: {0}'.format(object.category))
     try:
         doc = object.conf_sets['__document__'].data['license']
         result.append(':{0}: {1}'.format('License', doc))
@@ -127,15 +130,6 @@ def format_component(object, tree):
         result.append('')
     except KeyError:
         pass
-
-    result += section('Ports', 1)
-    result += format_ports(object)
-    result.append('')
-
-    result += section('Configuration parameters', 1)
-    result += format_properties(object)
-    result.append('')
-
     try:
         doc = object.conf_sets['__document__'].data['usage']
         result += section('Usage', 1)
@@ -143,6 +137,15 @@ def format_component(object, tree):
         result.append('')
     except KeyError:
         pass
+
+    result += section('Ports', 1)
+    result += get_ports_docs(object)
+    result.append('')
+
+    result += section('Configuration parameters', 1)
+    result += get_props_docs(object)
+    result.append('')
+
     try:
         doc = object.conf_sets['__documentation__'].data['misc']
         result += section('Miscellaneous information', 1)
@@ -161,15 +164,11 @@ def format_component(object, tree):
     return result
 
 
-def cat_target(cmd_path, full_path, options, tree=None):
-    result = []
+def get_docs(cmd_path, full_path, options, tree=None):
     path, port = rtctree.path.parse_path(full_path)
     if not path[-1]:
         # There was a trailing slash
-        trailing_slash = True
-        path = path[:-1]
-    else:
-        trailing_slash = False
+        raise rts_exceptions.NotAComponentError(cmd_path)
 
     if not tree:
         tree = rtctree.tree.create_rtctree(paths=path)
@@ -179,28 +178,25 @@ def cat_target(cmd_path, full_path, options, tree=None):
     object = tree.get_node(path)
 
     if object.is_component:
-        if trailing_slash:
-            raise rts_exceptions.NoSuchObjectError(cmd_path)
-        result += format_component(object, tree)
+        return get_comp_docs(object, tree)
     elif object.is_zombie:
         raise rts_exceptions.ZombieObjectError(cmd_path)
     else:
-        raise rts_exceptions.NoSuchObjectError(cmd_path)
-    return result
+        raise rts_exceptions.NotAComponentError(cmd_path)
 
 
 def main(argv=None, tree=None):
-    usage = '''Usage: %prog [options] [path]
-Display document about a component in specified (rst, html or latex) format.
-''' + rtshell.RTSH_PATH_USAGE
+    usage = '''Usage: %prog [options] <path>
+Display component documentation.'''
     version = rtshell.RTSH_VERSION
     parser = optparse.OptionParser(usage=usage, version=version)
+    parser.add_option('-f', '--format', dest='format', type='choice',
+            choices=('rst', 'html', 'latex'), default='html',
+            help='Output format (one of "rst", "html" or "latex"). '
+            '[Default: %default]')
     parser.add_option('-v', '--verbose', dest='verbose', action='store_true',
             default=False,
             help='Output verbose information. [Default: %default]')
-    parser.add_option('-f', '--format', dest='format', type='choice',
-            choices=('rst', 'html', 'latex'), default='html',
-            help='Output format (one of "rst", "html" or "latex"). [Default: %default]')
 
     if argv:
         sys.argv = [sys.argv[0]] + argv
@@ -212,7 +208,8 @@ Display document about a component in specified (rst, html or latex) format.
 
     if not args:
         # If no path given then can't do anything.
-        print >>sys.stderr, '{0}: Cannot cat a directory.'.format(sys.argv[0])
+        print >>sys.stderr, '{0}: No component specified.'.format(
+                os.path.basename(sys.argv[0]))
         return 1
     elif len(args) == 1:
         cmd_path = args[0]
@@ -222,15 +219,15 @@ Display document about a component in specified (rst, html or latex) format.
     full_path = path.cmd_path_to_full_path(cmd_path)
 
     try:
-        result = cat_target(cmd_path, full_path, options, tree=tree)
+        docs = '\n'.join(get_docs(cmd_path, full_path, options, tree=tree))
         if options.format == 'rst':
-            print '\n'.join(result)
+            print docs
         else:
-            print publish_string('\n'.join(result), writer_name=options.format)
+            print docutils.core.publish_string(docs, writer_name=options.format)
     except Exception, e:
         if options.verbose:
             traceback.print_exc()
-        print >>sys.stderr, '{0}: {1}'.format(sys.argv[0], e)
+        print >>sys.stderr, '{0}: {1}'.format(os.path.basename(sys.argv[0]), e)
         return 1
     return 0
 

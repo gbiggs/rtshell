8677da797912646c70386858571975fc22417d6a
Geoffrey Biggs <git@killbots.net>
2015-10-14 16:40:26 +0900

    Added note to compilation batch file

--- installer/compile.bat
+++ installer/compile.bat
@@ -1,7 +1,8 @@
 @echo off
 
-rem candle -ext WiXUtilExtension -dRTCTreeWheelPath="..\rtctree\dist\rtctree-4.1.0-py2-none-any.whl" -dRTSProfileWheelPath="..\rtsprofile\dist\rtsprofile-4.1.0-py2-none-any.whl" -pedantic -o rtshell.wixobj rtshell.wxs
+rem Do this first if necessary: pip install rtshell
 candle -ext WiXUtilExtension -pedantic -o rtshell.wixobj rtshell.wxs
 light -ext WixUtilExtension -pedantic -b ..\ rtshell.wixobj
 del rtshell.wixpdb
 del rtshell.wixobj
+rem Do this last if necessary: pip uninstall rtshell rtctree rtsprofile

172f2b2f2c5766354a03345bc26d5dd71449e065
Geoffrey Biggs <git@killbots.net>
2016-03-07 10:02:29 +0900

    Fixes #29: Use "call" in rtcwd.bat on Windows

--- data/rtcwd.bat
+++ data/rtcwd.bat
@@ -10,6 +10,6 @@ rem Licensed under the GNU Lesser General Public License version 3.
 rem http://www.gnu.org/licenses/lgpl-3.0.en.html
 
 python -c "import sys; import rtshell.rtcwd; sys.exit(rtshell.rtcwd.main(['%*']))" > settmp.bat
-settmp
+call settmp
 del settmp.bat
 
